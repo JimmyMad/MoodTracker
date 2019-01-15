7a00cfc19bc588a107c51a9259afee9680967f68
Jimmy <jimmy.maduro@hotmail.fr>
2019-01-15 13:10:55 +0100

    activity_main layout done Mood Class created

--- app/src/main/java/com/sweethome/jimmy/moodtracker/MainActivity.java
+++ app/src/main/java/com/sweethome/jimmy/moodtracker/MainActivity.java
@@ -2,6 +2,8 @@ package com.sweethome.jimmy.moodtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView img = (ImageView) findViewById(R.id.MainActivity_Smiley_ImageView);
+        LinearLayout background = (LinearLayout) findViewById(R.id.MainActivity_Background_LinearLayout);
+
+
+
+        img.setImageResource(R.drawable.ic_comment_black_48px);
+        background.getResources().getColor(R.color.light_sage);
     }
 }
---
+++ app/src/main/java/com/sweethome/jimmy/moodtracker/Mood.java
@@ -0,0 +1,12 @@
+package com.sweethome.jimmy.moodtracker;
+
+public class Mood {
+
+    private int moodId;
+    private int colorId;
+
+    public Mood(int moodId, int colorId) {
+        this.moodId = moodId;
+        this.colorId = colorId;
+    }
+}
